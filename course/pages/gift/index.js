2afbb23bb6a3f016d8c5aa9597e6821772ed18a5
wangyi <wetz1@163.com>
2018-03-19 01:11:24 +0800

    modify gift module

--- course/pages/gift/index.js
+++ course/pages/gift/index.js
@@ -9,8 +9,52 @@ Page({
    */
   data: {
     showPage: false,
-    price: "99.00",
-    type: "课程"
+    id:null,
+    price: "",
+    type: ""
+  },
+
+  /**
+   * 收下礼包操作
+   */
+  acceptGift:function(){
+    var apiUrl = '/Gift/acceptance';
+    var requestData = { id: this.data.id }
+    var myPromise = app._getApiData(apiUrl, requestData);
+    myPromise.then(data=>{
+      wx.hideLoading();
+      wx.showModal({
+        title: '领取成功',
+        content: '恭喜您成功领取好友送你的礼包，点击确定立即体验礼品内容',
+        showCancel:false,
+        success: function (res) {
+          if (res.confirm) {
+            wx.redirectTo({
+              url: data.info.url,
+            })
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    },reject=>{
+      wx.hideLoading();
+      wx.showModal({
+        title: '领取失败',
+        content: reject,
+        showCancel: false,
+        confirmText:"返回首页",
+        success: function (res) {
+          if (res.confirm) {
+            wx.reLaunch({
+              url: '/pages/index/index'
+            })
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    })
   },
 
   /**
@@ -20,7 +64,18 @@ Page({
     var apiUrl = '/Gift/index';
     var requestData = { key: options.key }
     var myPromise = app._fetchApiData(apiUrl, requestData);
-    // console.log("onload")
+    myPromise.then(data=>{
+      wx.hideLoading();
+      this.setData({
+        showPage:true,
+        id:data.info.id,
+        price:data.info.value,
+        type:data.info.pro_type == 1?"专栏":"课程"
+      })
+    },reject=>{
+    console.log(reject)
+    })
+   
   },
   onReady:function(){
 
