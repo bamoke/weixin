3364853005986f7017e82505617332bfccfa6557
bamoke <bamoke@163.com>
2018-09-27 22:14:08 +0800

    modufy onhre 09-27

---
+++ one/components/answer/answer.js
@@ -0,0 +1,72 @@
+// compenents/answer/answer.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    question:{
+      type:Object
+    },
+    index:{
+      type:Number
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    symbolArr: ['A', 'B', 'C', "D", 'E', 'F', 'G'],
+    answerResult:'',
+    isSelected:false,
+    curSeleted:''//当前选择
+  },
+
+  /**
+   * 组件的生命周期
+   */
+  ready: function () {
+    
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+    //选择答案
+    selectAnswer:function(e){
+      const value = e.detail.value
+      const questionType = this.data.question.type
+      const correctStr = this.data.question.correct
+      var answerResult;
+      if(questionType !== 2) {
+        answerResult = correctStr == value.join(",") ? 'correct' : "error",
+        this.setData({
+          isSelected:true,
+          answerResult,
+          curSeleted:value.join(",")
+        })
+        this.triggerEvent("confirm-answer", answerResult)
+      }else {
+        this.setData({
+          curSeleted: value.join(",")
+        })
+      }
+    },
+    // 多选确认答案
+    confirmAnswer:function(e){
+      if (this.data.curSeleted.length === 0) {
+        wx.showToast({
+          title: '请选择答案',
+          image: '/static/images/icon-error.png'
+        })
+        return
+      }
+      var seletctedValue = this.data.curSeleted
+      const correctStr = this.data.question.correct 
+      var answerResult = correctStr === seletctedValue ?'correct':"error"
+      this.setData({ isSelected: true, answerResult})
+      this.triggerEvent("confirm-answer", answerResult)
+    }
+  }
+})
--- one/pages/work/detail/index.js
+++ one/pages/work/detail/index.js
@@ -7,24 +7,64 @@ Page({
    */
   data: {
     pageShow:false,
-    symbolArr: ['A', 'B', 'C', "D", 'E', 'F', 'G'],
     showStartPage: true,
     showCompletePage: false,
-    curIndex: 0,
-    totalNum: 5,
-    correctNum: 0,
-    score: 0,
-    correctName: '',
-    errorList: [],
-    examinationInfo: {},
-    question: []
+    canShowNext:false,
+    totalNum:0,
+    curIndex: 0,//当前问答Index
+    score: 0,//总分
+    baseInfo: {},//基本信息
+    question: [],//问题列表
+    anserResult:[]//回答结果列表;
+  },
+
+  /**
+   * 确认答案
+   */
+  confimAnswer:function(e){
+    const curIndex = this.data.curIndex
+    const result = e.detail
+    const anserResult = this.data.anserResult
+    anserResult[curIndex] = result
+    this.setData({
+      anserResult,
+      canShowNext:true,
+    })
+  },
+  // next
+  nextQuestion:function(){
+    if (!this.data.canShowNext) return
+    this.setData({
+      curIndex:this.data.curIndex+1,
+      canShowNext:false
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const workId = options.id
+    const params = {
+      apiUrl: "/Work/detail",
+      requestData: { id: workId },
+      requestMethod: "GET"
+    }
+    const ajaxRequest = util.request(params)
+    ajaxRequest.then(res => {
+      var anserResult = res.data.question.map((item,index)=>{
+        if(index === 0) return 'active'
+        return ''
+      })
+      this.setData({
+        workId,
+        showPage: true,
+        baseInfo: res.data.baseInfo,
+        question: res.data.question,
+        totalNum: res.data.question.length,
+        anserResult
+      })
+    })
   },
 
   /**
